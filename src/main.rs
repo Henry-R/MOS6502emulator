bb3b5af885451e9aa89f37ebda8e80b745b94bda
HRW221 <hrw221@exeter.ac.uk>
2024-10-25 20:30:30 +0100

    Defined an instruction table that transforms the register state

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 
-
-
-struct Emulator {
-    // Registers
-    A: i8,      // Accumulator
-    P: i8,      // Status register
-    PC: i16,    // Program counter
-    S: i8,      // Stack pointer
-    X: i8,      // Index register
-    Y: i8      // Index register
+struct RegisterState {
+    pub A: i8,      // Accumulator
+    pub P: i8,      // Status register
+    pub PC: i16,    // Program counter
+    pub S: i8,      // Stack pointer
+    pub X: i8,      // Index register
+    pub Y: i8      // Index register
 }
 
-impl Emulator {
-    fn decode(opcode: u8) {
-        // Defines the type of instruction
-        let opcode = (opcode | 0b11100000) >> 5;
-        // Defines the addressing mode (intermediate, zero page, ...)
-        let address_mode = (opcode | 0b00011100) >> 2;
-        // Specifies the exact version of the instruction
-        let opcode_mode = opcode | 0b00000011;
+fn decode(opcode: u8) {
+    // Defines the type of instruction
+    let opcode = (opcode | 0b11100000) >> 5;
+    // Defines the addressing mode (intermediate, zero page, ...)
+    let address_mode = (opcode | 0b00011100) >> 2;
+    // Specifies the exact version of the instruction
+    let opcode_mode = opcode | 0b00000011;
 
 
-    }
 }
 
 fn main() {
+    let instruction_table: [fn(RegisterState) -> RegisterState; 8];
+
     println!("Hello, world!");
 }
