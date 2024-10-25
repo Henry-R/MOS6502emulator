07661b8d6e047f3b246bf081b3d7a0ee4f6c6c34
HRW221 <hrw221@exeter.ac.uk>
2024-10-25 21:28:14 +0100

    Changed Status register to use flags instead of the placeholder integer type

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 mod operations;
 
+struct StatusFlags {
+    // (n) Negative
+    pub n: bool,
+    // (v) Overflow
+    pub v: bool,
+    // (b) Break
+    pub b: bool,
+    // (d) Decimal
+    pub d: bool,
+    // (i) Interrupt disable
+    pub i: bool,
+    // (z) Zero
+    pub z: bool,
+    // (c) Carry
+    pub c: bool,
+}
+
 struct ComputerState {
     // REGISTERS
     // (A) Accumulator
     pub acc: i8,
     // (P) Status register
-    pub sta: i8,
+    pub sta: StatusFlags,
     // (PC) Program counter
     pub pc: i16,
     // (S) Stack pointer
