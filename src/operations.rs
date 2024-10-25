0a750cbdc7634ebd9e59a0b9acc0f21292d2ed78
HRW221 <hrw221@exeter.ac.uk>
2024-10-25 21:34:58 +0100

    Created stub BRK instruction

--- src/operations.rs
+++ src/operations.rs
@@ -2,9 +2,24 @@ use crate::ComputerState;
 
 // MOS6502 operation. Mutates the computer's state
 type MosOp = fn(&mut ComputerState);
-const INSTRUCTION_TABLE: [[MosOp; 16]; 16] = {
-    {
-        // BRK
-        MosOp {}
-    }
-};
+
+/// BRK (Force Break)
+///
+/// BRK initiates a software interrupt similar to a hardware
+/// interrupt (IRQ). The return address pushed to the stack is
+/// PC+2, providing an extra byte of spacing for a break mark
+/// (identifying a reason for the break.)
+/// The status register will be pushed to the stack with the break
+/// flag set to 1. However, when retrieved during RTI or by a PLP
+/// instruction, the break flag will be ignored.
+/// The interrupt disable flag is not set automatically.
+fn force_break(state: &mut ComputerState) {
+    // state.stk = state.pc + 2; RESEARCH TYPE OF STACK
+    state.sta.b = true;
+}
+
+const INSTRUCTION_TABLE: [[MosOp; 1]; 1] = [
+    [
+        force_break
+    ]
+];
