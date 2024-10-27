0f82616311e3960aa068b8fc6149b0c9479cee77
HRW221 <hrw221@exeter.ac.uk>
2024-10-27 11:16:48 +0000

    Added memory array and basic stack functions

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,56 @@ struct StatusFlags {
     pub c: bool,
 }
 
-struct ComputerState {
-    // REGISTERS
+
+struct Registers {
     // (A) Accumulator
-    pub acc: i8,
+    pub acc: u8,
     // (P) Status register
     pub sta: StatusFlags,
     // (PC) Program counter
-    pub pc: i16,
+    pub pc: u16,
     // (S) Stack pointer
-    pub stk: i8,
+    pub stk: u8,
     // (X) Index register
-    pub x: i8,
+    pub x: u8,
     // (Y) Index register
-    pub y: i8
+    pub y: u8
+}
+
+struct ComputerState {
+    // MEMORY
+    // Each page is 256 bytes
+    // First page is reserved for the Zero-Page ($0000-$00FF)
+    // Second page is reserved for system stack ($0100-$01FF)
+    // Last 6 bytes are reserved for interrupts ($FFFA-$FFFF)
+    mem: [u8; 2^16],
+
+    regs: Registers,
+}
+
+impl ComputerState {
+    // Important memory locations
+    const ZERO_PAGE: u16 = 0x0100;
+    const STACK_PAGE: u16 = 0x0100;
+
+    // STACK INSTRUCTIONS
+    /// Returns the byte at the top of the stack without mutating memory
+    fn stk_peek(& self) -> u8 {
+        self.mem[Self::STACK_PAGE + self.regs.stk as u16]
+    }
+
+    /// Returns the byte at the top of the stack and reduces the stack pointer by one
+    fn stk_pop(&mut self) -> u8 {
+        let ret = self.stk_peek();
+        self.regs.stk += 1;
+        ret
+    }
+
+    /// Increases the stack pointer by one, and pushed the supplied byte onto this stack location
+    fn stk_push(&mut self, byte: u8){
+        self.regs.stk -= 1;
+        self.mem[Self::STACK_PAGE + self.regs.stk as u16] = byte;
+    }
 }
 
 fn main() {
