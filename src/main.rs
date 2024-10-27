803c158f2d82b565e2e97a6a00557efa7ac17c5b
HRW221 <hrw221@exeter.ac.uk>
2024-10-27 10:22:09 +0000

    Removed spaces between StatusFlags members

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,16 @@ mod operations;
 struct StatusFlags {
     // (n) Negative
     pub n: bool,
-
     // (v) Overflow
     pub v: bool,
-
     // (b) Break
     pub b: bool,
-
     // (d) Decimal
     pub d: bool,
-
     // (i) Interrupt disable
     pub i: bool,
-
     // (z) Zero
     pub z: bool,
-
     // (c) Carry
     pub c: bool,
 }
