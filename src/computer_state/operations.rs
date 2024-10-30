d26a30f2c65dfea637902716358117efbe048597
HRW221 <hrw221@exeter.ac.uk>
2024-10-30 12:18:43 +0000

    Added stub modules for other operation types

--- src/computer_state/operations.rs
+++ src/computer_state/operations.rs
@@ -1,8 +1,10 @@
 use crate::computer_state::ComputerState;
 use crate::computer_state::operations::interrupt::*;
 
-mod add;
+mod arithmetic;
 mod interrupt;
+mod branch;
+mod bitwise;
 
 pub const INSTRUCTION_TABLE: [fn (&mut ComputerState); 16 * 16] = [
     brk, nop, nop, nop, nop, nop, nop, nop, nop, nop, nop, nop, nop, nop, nop, nop,
