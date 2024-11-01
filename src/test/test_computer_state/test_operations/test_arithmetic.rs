943577633e46ae81c7d59d372fc687d4f74f1d20
HRW221 <hrw221@exeter.ac.uk>
2024-11-01 18:57:56 +0000

    Added credit to tdinucci

--- src/test/test_computer_state/test_operations/test_arithmetic.rs
+++ src/test/test_computer_state/test_operations/test_arithmetic.rs
@@ -4,15 +4,20 @@ mod tests {
     use crate::computer_state::{ComputerState};
     use crate::computer_state::operations::arithmetic::*;
 
+    // Unit tests shamelessly taken from https://github.com/tdinucci/6502-emulator
+    // All credit for coming up with the test cases goes to tdinucci
+
     #[test]
     fn test_adc_im() {
         let mut state: ComputerState = ComputerState::new();
 
-        let old_pc = state.regs.pc;
+        // Initialise computer state
         state.regs.acc = 36;
         state.insert_operation_at_pc(adc_im);
         state.insert_at_pc(36);
-        state.regs.pc = old_pc;
+
+        // Reset pc for execution
+        state.regs.pc = 0;
 
         state.execute_next();
 
