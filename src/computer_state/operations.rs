3fc38e124856f6d591df11efd87b397522b57c2a
HRW221 <hrw221@exeter.ac.uk>
2024-11-01 18:29:27 +0000

    Cleaned up INSTRUCTION_TABLE

--- src/computer_state/operations.rs
+++ src/computer_state/operations.rs
@@ -10,7 +10,7 @@ mod branch;
 mod bitwise;
 mod flags;
 
-const INSTRUCTION_TABLE: [fn (&mut ComputerState); 256] = [
+const INSTRUCTION_TABLE: [fn (&mut ComputerState); u8::MAX as usize] = [
  //      0         2         4         6         8         A         C         E
  //           1         3         5         7         9         B         D         F
  /* 0 */ brk,      nop,      nop,      asl_zp,   nop,      asl_acc,  nop,      asl_ab,
@@ -26,9 +26,9 @@ const INSTRUCTION_TABLE: [fn (&mut ComputerState); 256] = [
  /* 5 */ brk,      nop,      nop,      lsr_zpx,  cli,      nop,      nop,      lsr_abx,
               eor_iny,  nop,      eor_zpx,  nop,      eor_aby,  nop,      eor_abx,  nop,
  /* 6 */ brk,      nop,      nop,      nop,      nop,      nop,      nop,      nop,
-    adc_inx,  nop, adc_zp,   nop, adc_im,   nop, adc_ab,   nop,
+              adc_inx,  nop,      adc_zp,   nop,      adc_im,   nop,      adc_ab,   nop,
  /* 7 */ brk,      nop,      nop,      nop,      sei,      nop,      nop,      nop,
-    adc_iny,  nop, adc_zpx,  nop, adc_aby,  nop, adc_abx,  nop,
+              adc_iny,  nop,      adc_zpx,  nop,      adc_aby,  nop,      adc_abx,  nop,
  /* 8 */ brk,      nop,      nop,      nop,      dey,      nop,      nop,      nop,
               nop,      nop,      nop,      nop,      nop,      nop,      nop,      nop,
  /* 9 */ brk,      nop,      nop,      nop,      nop,      nop,      nop,      nop,
@@ -42,9 +42,9 @@ const INSTRUCTION_TABLE: [fn (&mut ComputerState); 256] = [
  /* D */ brk,      nop,      nop,      dec_zpx,  cld,      nop,      nop,      dec_abx,
               nop,      nop,      nop,      nop,      nop,      nop,      nop,      nop,
  /* E */ brk,      nop,      nop,      inc_zp,   inx,      nop,      nop,      inc_ab,
-    sbc_inx,  nop, sbc_zp,   nop, sbc_im,   nop, sbc_ab,   nop,
+              sbc_inx,  nop,      sbc_zp,   nop,      sbc_im,   nop,      sbc_ab,   nop,
  /* F */ brk,      nop,      nop,      inc_zpx,  sed,      nop,      nop,      inc_abx,
-    sbc_iny,  nop, sbc_zpx,  nop, sbc_aby,  nop, sbc_abx,  nop,
+         sbc_iny,       nop,      sbc_zpx,  nop,      sbc_aby,  nop,      sbc_abx,  nop,
 ];
 
 pub const fn decode(opcode: u8) -> fn (&mut ComputerState) {
