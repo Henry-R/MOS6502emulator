66042defc78d36b8ae74173b0f0138cfc9473e14
HRW221 <hrw221@exeter.ac.uk>
2024-11-08 16:24:55 +0000

    STA, STX, and STY now set Z, N flags

--- src/computer_state/operations/load_store.rs
+++ src/computer_state/operations/load_store.rs
@@ -1,124 +1,148 @@
 use crate::computer_state::ComputerState;
 use crate::computer_state::memory::Memory;
+use crate::computer_state::status_register::get_zero_neg_flags;
+
+fn lda(computer_state: &mut ComputerState, value: u8) {
+    computer_state.acc.set(value);
+    computer_state.sta |= get_zero_neg_flags(value);
+}
 
 /// LDA (immediate addressing mode)
 /// Opcode: A9
 pub fn lda_im(state: &mut ComputerState) {
-    state.acc.set(Memory::fetch_immediate(&mut state.mem))
+    let value = Memory::fetch_immediate(&mut state.mem);
+    lda(state, value);
 }
 
 /// LDA (zero-page addressing mode)
 /// Opcode: A5
 pub fn lda_zp(state: &mut ComputerState) {
-    state.acc.set(Memory::fetch_zero_page(&mut state.mem))
+    let value = Memory::fetch_zero_page(&mut state.mem);
+    lda(state, value);
 }
 
 /// LDA (zero-page X addressing mode)
 /// Opcode: B5
 pub fn lda_zpx(state: &mut ComputerState) {
-    state.acc.set(Memory::fetch_zero_page_x(&mut state.mem))
+    let value = Memory::fetch_zero_page_x(&mut state.mem);
+    lda(state, value);
 }
 
 /// LDA (absolute addressing mode)
-/// Opcode: Ad
+/// Opcode: AD
 pub fn lda_ab(state: &mut ComputerState) {
-    state.acc.set(Memory::fetch_absolute(&mut state.mem))
+    let value = Memory::fetch_absolute(&mut state.mem);
+    lda(state, value);
 }
 
 /// LDA (absolute X addressing mode)
 /// Opcode: BD
 pub fn lda_abx(state: &mut ComputerState) {
-    state.acc.set(Memory::fetch_absolute_x(&mut state.mem))
+    let value = Memory::fetch_absolute_x(&mut state.mem);
+    lda(state, value);
 }
 
 /// LDA (absolute Y addressing mode)
 /// Opcode: B9
 pub fn lda_aby(state: &mut ComputerState) {
-    state.acc.set(Memory::fetch_absolute_y(&mut state.mem))
+    let value = Memory::fetch_absolute_y(&mut state.mem);
+    lda(state, value);
 }
 
 /// LDA (indirect X addressing mode)
 /// Opcode: A1
 pub fn lda_inx(state: &mut ComputerState) {
-    state.acc.set(Memory::fetch_indirect_x(&mut state.mem))
+    let value = Memory::fetch_indirect_x(&mut state.mem);
+    lda(state, value);
 }
 
 /// LDA (indirect Y addressing mode)
 /// Opcode: B1
 pub fn lda_iny(state: &mut ComputerState) {
-    state.acc.set(Memory::fetch_indirect_y(&mut state.mem))
+    let value = Memory::fetch_indirect_y(&mut state.mem);
+    lda(state, value);
 }
 
+
+fn ldx(state: &mut ComputerState, value: u8) {
+    state.set_x(usize::from(value));
+    state.sta |= get_zero_neg_flags(value);
+}
 
 /// LDX (immediate addressing mode)
 /// Opcode: A2
 pub fn ldx_im(state: &mut ComputerState) {
     let value = Memory::fetch_immediate(&mut state.mem);
-    state.set_x(usize::from(value))
+    ldx(state, value);
 }
 
 /// LDX (zero-page addressing mode)
 /// Opcode: A6
 pub fn ldx_zp(state: &mut ComputerState) {
     let value = Memory::fetch_zero_page(&mut state.mem);
-    state.set_x(usize::from(value))
+    ldx(state, value);
 }
 
 /// LDX (zero-page Y addressing mode)
 /// Opcode: B6
 pub fn ldx_zpy(state: &mut ComputerState) {
     let value = Memory::fetch_zero_page_y(&mut state.mem);
-    state.set_x(usize::from(value))
+    ldx(state, value);
 }
 
 /// LDX (absolute addressing mode)
 /// Opcode: AE
 pub fn ldx_ab(state: &mut ComputerState) {
     let value = Memory::fetch_absolute(&mut state.mem);
-    state.set_x(usize::from(value))
+    ldx(state, value);
 }
 
 /// LDX (absolute Y addressing mode)
 /// Opcode: BE
 pub fn ldx_aby(state: &mut ComputerState) {
     let value = Memory::fetch_absolute_y(&mut state.mem);
-    state.set_x(usize::from(value))
+    ldx(state, value);
 }
 
+
+fn ldy(state: &mut ComputerState, value: u8) {
+    state.set_y(usize::from(value));
+    state.sta |= get_zero_neg_flags(value);
+}
 
 /// LDY (immediate addressing mode)
 /// Opcode: A2
 pub fn ldy_im(state: &mut ComputerState) {
     let value = Memory::fetch_immediate(&mut state.mem);
-    state.set_y(usize::from(value))
+    ldy(state, value);
 }
 
 /// LDY (zero-page addressing mode)
 /// Opcode: A6
 pub fn ldy_zp(state: &mut ComputerState) {
     let value = Memory::fetch_zero_page(&mut state.mem);
-    state.set_y(usize::from(value))
+    ldy(state, value);
 }
 
 /// LDY (zero-page X addressing mode)
 /// Opcode: B6
 pub fn ldy_zpy(state: &mut ComputerState) {
     let value = Memory::fetch_zero_page_x(&mut state.mem);
-    state.set_y(usize::from(value))
+    ldy(state, value);
 }
 
 /// LDY (absolute addressing mode)
 /// Opcode: AE
 pub fn ldy_ab(state: &mut ComputerState) {
     let value = Memory::fetch_absolute(&mut state.mem);
-    state.set_y(usize::from(value))
+    ldy(state, value);
 }
 
 /// LDY (absolute X addressing mode)
 /// Opcode: BE
 pub fn ldy_aby(state: &mut ComputerState) {
     let value = Memory::fetch_absolute_x(&mut state.mem);
-    state.set_y(usize::from(value))
+    ldy(state, value);
 }
 
 
