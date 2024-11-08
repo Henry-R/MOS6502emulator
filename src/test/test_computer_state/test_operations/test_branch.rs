818f6e560b7e84c88103ade18af69fac48e0a577
HRW221 <hrw221@exeter.ac.uk>
2024-11-08 15:27:10 +0000

    tests for branch

--- src/test/test_computer_state/test_operations/test_branch.rs
+++ src/test/test_computer_state/test_operations/test_branch.rs
@@ -4,7 +4,7 @@ use crate::computer_state::operations::opcode_from_operation;
 use crate::computer_state::status_register::StatusRegister;
 
 #[test]
-fn test_brc_forward() {
+fn test_bcs_forward() {
     let mut state = ComputerState::new();
     let old_pc = state.mem.pc.get();
     state.sta |= StatusRegister::C;
@@ -18,7 +18,7 @@ fn test_brc_forward() {
 }
 
 #[test]
-fn test_brc_backwards() {
+fn test_bcs_backwards() {
     let mut state = ComputerState::new();
     let old_pc = state.mem.pc.get();
     state.sta |= StatusRegister::C;
@@ -28,16 +28,298 @@ fn test_brc_backwards() {
     ]);
     state.execute_next();
 
-    // TODO double check 0x10 is the correct offset
     assert_eq!(old_pc + 2u16.wrapping_sub(0x10) as usize, state.mem.pc.get());
 }
 
 #[test]
-fn test_brc_not_take() {
+fn test_bcs_not_take() {
     let mut state = ComputerState::new();
     let old_pc = state.mem.pc.get();
     state.set_up_state(&vec![
         opcode_from_operation(bcs),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2, state.mem.pc.get());
+}
+
+#[test]
+fn test_bcc_forward() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bcc),
+        0x23
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2 + 0x23, state.mem.pc.get());
+}
+
+#[test]
+fn test_bcc_backwards() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bcc),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2u16.wrapping_sub(0x10) as usize, state.mem.pc.get());
+}
+
+#[test]
+fn test_bcc_not_take() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::C;
+    state.set_up_state(&vec![
+        opcode_from_operation(bcc),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2, state.mem.pc.get());
+}
+
+#[test]
+fn test_beq_forward() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::Z;
+    state.set_up_state(&vec![
+        opcode_from_operation(beq),
+        0x23
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2 + 0x23, state.mem.pc.get());
+}
+
+#[test]
+fn test_beq_backwards() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::Z;
+    state.set_up_state(&vec![
+        opcode_from_operation(beq),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2u16.wrapping_sub(0x10) as usize, state.mem.pc.get());
+}
+
+#[test]
+fn test_beq_not_take() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(beq),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2, state.mem.pc.get());
+}
+
+#[test]
+fn test_bne_forward() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bne),
+        0x23
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2 + 0x23, state.mem.pc.get());
+}
+
+#[test]
+fn test_bne_backwards() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bne),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2u16.wrapping_sub(0x10) as usize, state.mem.pc.get());
+}
+
+#[test]
+fn test_bne_not_take() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::Z;
+    state.set_up_state(&vec![
+        opcode_from_operation(bne),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2, state.mem.pc.get());
+}
+
+#[test]
+fn test_bpl_forward() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bpl),
+        0x23
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2 + 0x23, state.mem.pc.get());
+}
+
+#[test]
+fn test_bpl_backwards() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bpl),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2u16.wrapping_sub(0x10) as usize, state.mem.pc.get());
+}
+
+#[test]
+fn test_bpl_not_take() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::N;
+    state.set_up_state(&vec![
+        opcode_from_operation(bpl),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2, state.mem.pc.get());
+}
+
+#[test]
+fn test_bmi_forward() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::N;
+    state.set_up_state(&vec![
+        opcode_from_operation(bmi),
+        0x23
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2 + 0x23, state.mem.pc.get());
+}
+
+#[test]
+fn test_bmi_backwards() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::N;
+    state.set_up_state(&vec![
+        opcode_from_operation(bmi),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2u16.wrapping_sub(0x10) as usize, state.mem.pc.get());
+}
+
+#[test]
+fn test_bmi_not_take() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bmi),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2, state.mem.pc.get());
+}
+
+#[test]
+fn test_bvs_forward() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::V;
+    state.set_up_state(&vec![
+        opcode_from_operation(bvs),
+        0x23
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2 + 0x23, state.mem.pc.get());
+}
+
+#[test]
+fn test_bvs_backwards() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::V;
+    state.set_up_state(&vec![
+        opcode_from_operation(bvs),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2u16.wrapping_sub(0x10) as usize, state.mem.pc.get());
+}
+
+#[test]
+fn test_bvs_not_take() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bvs),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2, state.mem.pc.get());
+}
+
+#[test]
+fn test_bvc_forward() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bvc),
+        0x23
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2 + 0x23, state.mem.pc.get());
+}
+
+#[test]
+fn test_bvc_backwards() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.set_up_state(&vec![
+        opcode_from_operation(bvc),
+        0xF0 // -16
+    ]);
+    state.execute_next();
+
+    assert_eq!(old_pc + 2u16.wrapping_sub(0x10) as usize, state.mem.pc.get());
+}
+
+#[test]
+fn test_bvc_not_take() {
+    let mut state = ComputerState::new();
+    let old_pc = state.mem.pc.get();
+    state.sta |= StatusRegister::V;
+    state.set_up_state(&vec![
+        opcode_from_operation(bvc),
         0xF0 // -16
     ]);
     state.execute_next();
