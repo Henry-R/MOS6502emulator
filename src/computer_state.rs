b4d93b974c10b151972a559a2377ae56d2148064
HRW221 <hrw221@exeter.ac.uk>
2024-11-08 11:13:56 +0000

    ComputerState::get_state_str for displaying current state of computer

--- src/computer_state.rs
+++ src/computer_state.rs
@@ -29,6 +29,47 @@ impl ComputerState {
         }
     }
 
+    // DISPLAY FUNCTIONS
+    pub fn get_state_str(&self) -> String {
+        // Registers
+        let program_counter = self.mem.pc.get();
+        //let stack_pointer = stringify!(self.mem.pc.get());
+        let accumulator = self.acc.get();
+        let index_x = self.mem.x;
+        let index_y = self.mem.y;
+
+        // Processor Status
+        let carry_flag =     self.sta.flag_value(StatusRegister::C);
+        let zero_flag =      self.sta.flag_value(StatusRegister::Z);
+        let interrupt_flag = self.sta.flag_value(StatusRegister::I);
+        let decimal_flag =   self.sta.flag_value(StatusRegister::D);
+        let break_flag =     self.sta.flag_value(StatusRegister::B);
+        let overflow_flag =  self.sta.flag_value(StatusRegister::V);
+        let negative_flag =  self.sta.flag_value(StatusRegister::N);
+
+        let pc = self.mem.pc.get();
+        let opcode = self.mem.fetch_byte_from_addr(pc);
+        let instruction = operations::decode_operation_name(opcode);
+
+        format!(
+            "CURRENT INSTRUCTION:
+                instruction: {instruction}
+                opcode:      {opcode}
+            REGISTERS:
+            Program Counter: {program_counter}
+            Accumulator:     {accumulator}
+            Index X:         {index_x}
+            Index Y:         {index_y}
+            Status Flags:
+                Carry:       {carry_flag}
+                Zero:        {zero_flag}
+                Interrupt:   {interrupt_flag}
+                Decimal:     {decimal_flag}
+                Break:       {break_flag}
+                Overflow:    {overflow_flag}
+                Negative:    {negative_flag}")
+    }
+
     // REGISTERS
     pub fn get_x(&self) -> usize {
         self.mem.x
